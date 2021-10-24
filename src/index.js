d97e8883d7e5afb93275d8cd6b820ffab638dd31
Dhruv <dhruvpurwar15@gmail.com>
2021-10-24 16:14:35 +0530

    Changed cors address to 3001

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app=express();
 const port= process.env.PORT|| 3000;
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentinals: true
   }))
 

0f5dd1668d939d50aaf0f4cd641209c3bf8f214c
Dhruv <dhruvpurwar15@gmail.com>
2021-10-29 21:27:01 +0530

    Added get routes in eval models

--- src/routers/eval.js
+++ src/routers/eval.js
@@ -3,6 +3,27 @@ const router = new express.Router();
 const eval= require('../models/eval');
 
 
+router.get('/eval/:id',async(req,res)=>{
+    const mentorID= req.params.id;
+    try {
+        const evals= await eval.find({mentorID});
+        res.status(200).send(evals);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+router.get('/evals/:sid',async(req,res)=>{
+    const SID= req.params.sid;
+    try {
+        const eval= await eval.findOne({SID});
+        res.status(200).send(eval);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+
 
 
 

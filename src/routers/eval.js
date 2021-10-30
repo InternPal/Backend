6586369fb6bdbcd2df0420d2bedb8004d3117d52
Dhruv <dhruvpurwar15@gmail.com>
2021-10-31 00:22:39 +0530

    Added mid,final report route & panel , mentor grade

--- src/routers/eval.js
+++ src/routers/eval.js
@@ -48,6 +48,71 @@ router.post('/eval',async(req,res)=>{
 })
 
 
+router.post('/eval/midReport',async(req,res)=>{
+    const SID= req.body.SID;
+    const string= req.body.string;
+
+    try {
+        
+        const eval= await eval.findOne({SID});
+    eval.midtermReport= string;
+    await eval.save();
+    res.status(200).send(eval);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+
+    
+})
+
+router.post('/eval/finalReport',async(req,res)=>{
+    const SID= req.body.SID;
+    const string= req.body.string;
+
+    try {
+        
+        const eval= await eval.findOne({SID});
+    eval.finalReport= string;
+    await eval.save();
+    res.status(200).send(eval);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+
+    
+})
+
+router.post('/eval/mentorGrade',async(req,res)=>{
+    const SID= req.body.SID;
+    const number= req.body.number;
+
+    try {
+        
+        const eval= await eval.findOne({SID});
+    eval.mentorGrade= number;
+    await eval.save();
+    res.status(200).send(eval);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
+
+router.post('/eval/panelGrade',async(req,res)=>{
+    const SID= req.body.SID;
+    const number= req.body.number;
+
+    try {
+        
+        const eval= await eval.findOne({SID});
+        eval.panelGrade= number;
+        await eval.save();
+    res.status(200).send(eval);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
 
 
 

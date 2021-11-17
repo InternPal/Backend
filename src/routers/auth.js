ed44ac67cb9adb6b8178898a9c441d6e05831323
Dhruv <dhruvpurwar15@gmail.com>
2021-11-18 01:46:15 +0530

    updated logins

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,6 +4,8 @@ const Student= require('../models/student');
 const Mentor= require('../models/mentor');
 const Admin= require('../models/admin');
 const Job= require('../models/job');
+const facultyCoordinator=require('../models/facultyCoordinator');
+const industryCoordinator=require('../models/industryCoordinator');
 
 
 
@@ -13,10 +15,14 @@ router.post('/login',async(req,res)=>{
         if(req.body.type ==='Student'){
             const student= await Student.findByCredentials(req.body.email,req.body.password);
             res.send(student);
-        }else if(req.body.type ==='Mentor'){
-            const mentor= await Mentor.findByCredentials(req.body.email,req.body.password);
-            res.send(mentor);
-        }else {
+        }else if(req.body.type ==='facultyCoordinator'){
+            const faculty= await facultyCoordinator.findByCredentials(req.body.email,req.body.password);
+            res.send(faculty);
+        }else if(req.body.type ==='industryCoordinator'){
+            const industry= await industryCoordinator.findByCredentials(req.body.email,req.body.password);
+            res.send(industry);
+        }
+        else {
             const admin= await Admin.findByCredentials(req.body.email,req.body.password);
             res.send(admin);
         }
